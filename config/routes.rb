f9d9395cf3cd8a7540898c8deee789fdc96a5f9e
hatruong219 <yeunguoicongaikoyeuanh@gmail.com>
2022-03-03 09:36:13 +0700

    chapter 6

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,8 @@ Rails.application.routes.draw do
     get "help"    => "static_pages#help"
     get "about"   => "static_pages#about"
     get "contact" => "static_pages#contact"
+    get "user/new"
   end
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
