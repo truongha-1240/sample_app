f93376e1eef0b665476e0c6431c5ec0cec74186c
hatruong219 <yeunguoicongaikoyeuanh@gmail.com>
2022-03-15 14:08:46 +0700

    Chapter 12

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
   validates :name, presence: true,
@@ -30,11 +30,29 @@ class User < ApplicationRecord
   end
 
   def activate
-    update_columns activated: true, activated_at: Time.zone
+    update_columns activated: true, activated_at: Time.zone.now
   end
 
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_columns reset_digest: User.digest(reset_token),
+                   reset_sent_at: Time.zone.now
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def password_reset_expired?
+    reset_sent_at < Settings.time.two_hours.hours.ago
+  end
+
+  def clear_password_reset
+    update_columns(reset_digest: nil, reset_sent_at: nil)
   end
 
   class << self
--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,5 @@ module SampleApp
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     config.i18n.available_locales = [:en, :vi]
     config.i18n.default_locale = :vi
-    config.time_zone = Settings.time_zone
   end
 end
